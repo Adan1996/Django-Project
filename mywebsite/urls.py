a38510e67c8d732b5390a0b063aa7ff315a81629
Adan1996 <syahdanmasyhuri@gmail.com>
2019-12-22 22:49:35 +0700

    menambahkan folder blog dan about sebagai app

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from . import views
+from blog import views as blogViews
+from about import views as aboutViews
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^about/$', views.about),
+    url(r'^admin/', admin.site.urls),
+    url(r'^blog/$', blogViews.index),
+    url(r'^about/$', aboutViews.index),
     url(r'^$', views.index),
 ]
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def about(request):
-    return render(request, 'about.html')
+# def about(request):
+#     return render(request, 'about.html')
