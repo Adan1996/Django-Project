1dcc7d7808a6f3af51d458eeaae8ec6509b07f77
Adan1996 <syahdanmasyhuri@gmail.com>
2020-03-26 13:21:40 +0700

    Model field, Tipe data, Options, dan Migrations

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=20)
     body = models.TextField()
+    email = models.EmailField(default='nama@mail.com') # default adalah options untuk memasukan nilai default email, karena field email tidak boleh kosong
+    alamat = models.CharField(max_length=200, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "{}".format(self.title)
