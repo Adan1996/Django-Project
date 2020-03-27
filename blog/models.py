48a800246dc83ab272ecdaef0337113a89fe4b44
Adan1996 <syahdanmasyhuri@gmail.com>
2020-03-27 12:39:23 +0700

    Model Object Relational Mapping dengan Python Shell

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=255)
     body = models.TextField()
-    email = models.EmailField(default='nama@mail.com') # default adalah options untuk memasukan nilai default email, karena field email tidak boleh kosong
-    alamat = models.CharField(max_length=200, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255)
+    # pembahasan
+    # Post.objects.create(id="value")
+    # Post.objects.all()
+    # Post.objects.all()[1].delete()
+
+    # a = Post.objects.all()[0]
+    ## a.title = "judul baru"
+    ## a.save()
+
+    # looping sederhana
+    # repr(semuaPost)
+    # len(semuaPost)
+
+    # email = models.EmailField(default='nama@mail.com') # default adalah options untuk memasukan nilai default email, karena field email tidak boleh kosong
+    # alamat = models.CharField(max_length=200, blank=True)
+    # date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{}".format(self.title)
+        return "{}. {}".format(self.id, self.title)
